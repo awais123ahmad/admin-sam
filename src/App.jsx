8fd1a64500b72f4aaea8bd19e3a8b3784886f141
awais123ahmad <awaisahmadajk@gmail.com>
2024-12-07 22:57:45 +0500

    admin

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/dispenser">
       <PortalLayout>
         <Routes>
           <Route path="/" element={<LoginPage />} />
