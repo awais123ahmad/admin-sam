6d4a563b26fe6e4bf6290a1161730b457b9f722a
awais123ahmad <awaisahmadajk@gmail.com>
2024-12-07 23:01:37 +0500

    r

--- src/Services/axiosInstance.js
+++ src/Services/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     //baseURL: 'https://dbsspareparts.com/api',
-     baseURL: 'http://localhost:5005/api',
+     baseURL: 'http://localhost/api',
 
     headers: {
         'Content-Type': 'application/json',
